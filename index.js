beb187305823cc166f0fd437557228176d8f9b74
aan4ndd <anandpryo@gmail.com>
2025-11-24 15:46:55 +0530

    added game logic

--- index.js
+++ index.js
@@ -3,12 +3,45 @@
 // write logic for 1 game 
 // add score 
 // write logic for best of 3
+let humanScore = 0;
+let computerScore = 0;
+let computerChoice;
+let humanChoice;
 
 
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
-    const computerChoice = Math.floor(Math.random() * choices.length);
-    return choices[computerChoice];
+    let randomIndex = Math.floor(Math.random() * choices.length);
+     computerChoice = choices[randomIndex];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+     humanChoice = prompt("Enter your choice :");
+}
+
+function playRound(humanSelection, computerSelection){
+    if(humanChoice == computerChoice){
+     return console.log("It's a Draw")
+    }
+    else if(
+        (humanChoice == "rock" && computerChoice == "scissors")||
+        (humanChoice == "paper" && computerChoice == "rock")||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ){
+        return console.log("You Win");
+        humanScore++;
+
+
+    }
+    else{
+        return console.log("You lose");
+        computerScore++;
+
+    }
+      
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
